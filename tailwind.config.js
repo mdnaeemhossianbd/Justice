1a7e1459271bdb94b7de2cc2d3634dace75a52ea
Munshi <habitsk50@gmail.com>
2026-01-18 03:30:55 +0300

    Navbar Banner Service Section done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/your-banner-image.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
